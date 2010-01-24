2e059ed31e629dd5323ffb4e3b9fc6519e73a391
Joel Boehland <joel.boehland@evocomputing.com>
2010-01-23 16:08:49 -0800

    Add comment about installing incanter from git. Remove some cruft from project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,14 @@
-(defproject com.evocomputing/cljr
+(defproject com.evocomputing/rincanter
   "1.0.0-SNAPSHOT"
   :description "Clojure/R integration using rjava"
-  ;;:repositories [["ubic"  "http://www.chibi.ubc.ca/maven2/"]]
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-                 [org.rosuda.REngine.JRI/JRIEngine "0.5.1"]
-                 [org.rosuda.REngine/REngine "0.5.1"]
+                 ;;commenting these out- they were made from my
+                 ;;local install
+                 ;;[org.rosuda.REngine.JRI/JRIEngine "0.5.1"]
+                 ;;[org.rosuda.REngine/REngine "0.5.1"]
+
+                 ;;this is incanter from git installed locally
                  [incanter/incanter-app "1.0-master-SNAPSHOT"]]
   :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]]
   ;:native-path "./native"
