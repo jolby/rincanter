c3988b2410338bf16789bfe8c57a153cc35dfada
Joel Boehland <joel.boehland@evocomputing.com>
2010-01-29 08:05:57 -0800

    Added license to test file.

--- test/com/evocomputing/test/rincanter.clj
+++ test/com/evocomputing/test/rincanter.clj
@@ -1,3 +1,15 @@
+;; by Joel Boehland http://github.com/jolby/rincanter
+;; January 24, 2010
+
+;; Copyright (c) Joel Boehland, 2010. All rights reserved.  The use
+;; and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this
+;; distribution.  By using this software in any fashion, you are
+;; agreeing to be bound by the terms of this license.  You must not
+;; remove this notice, or any other, from this software.
+
+
 (ns com.evocomputing.test.rincanter
   (:import (org.rosuda.REngine REXP RList REXPGenericVector
                                REXPInteger REXPDouble REXPString REXPLogical
