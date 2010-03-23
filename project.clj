c0aa78949bfc482eb2e0ecf0cd0407328dc9f09b
Joel Boehland <joel.boehland@evocomputing.com>
2010-03-23 12:47:48 -0700

    Added David Liebke's suggested changes to newer incanter spec and swank-clojure spec in dependencies.

--- project.clj
+++ project.clj
@@ -4,20 +4,12 @@
   :repositories {"incanter" "http://repo.incanter.org"}
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-                 ;;commenting these out- they were made from my
-                 ;;local install
-                 ;;[org.rosuda.REngine.JRI/JRIEngine "0.5.1"]
-                 ;;[org.rosuda.REngine/REngine "0.5.1"]
-
-                 ;;this is incanter from git installed locally
-                 [incanter/incanter-app "1.0-master-SNAPSHOT"]]
-  :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]
+                 [org.incanter/incanter-full "1.2.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.1.0"]
                      [autodoc "0.7.0"]]
-  :autodoc { :name "rincanter"
+  :autodoc {:name "rincanter"
             :description "Clojure/R integration using rosuda JRIEngine"
             :page-title "Rincanter API documentation"
             :copyright "Eclipse Public License (EPL)"
             :web-src-dir "http://github.com/jolby/rincanter/blob/"
-            :web-home "http://jolby.github.com/rincanter/"}
-  ;:native-path "./native"
-  )
+            :web-home "http://jolby.github.com/rincanter/"})
