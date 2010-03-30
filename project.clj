6f3ee20ee323137782eddc19392a13a9c78c5642
Joel Boehland <joel.boehland@evocomputing.com>
2010-03-30 16:08:49 -0700

    Added native-deps plugin dependency

--- project.clj
+++ project.clj
@@ -5,8 +5,10 @@
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
                  [org.incanter/incanter-full "1.2.0-SNAPSHOT"]]
+  :native-dependencies [[jriengine "0.8.4"]]
   :dev-dependencies [[swank-clojure "1.1.0"]
-                     [autodoc "0.7.0"]]
+                     [autodoc "0.7.0"]
+                     [native-deps "1.0.0"]]
   :autodoc {:name "rincanter"
             :description "Clojure/R integration using rosuda JRIEngine"
             :page-title "Rincanter API documentation"
