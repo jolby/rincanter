0b7f76b591768faeb9d36430f05104b4ba5f98cf
edmund <edmund@t.(none)>
2012-09-27 10:16:03 +0100

    Update for Clojure 1.4.0 and lein2 stack.

--- project.clj
+++ project.clj
@@ -1,12 +1,14 @@
 (defproject com.evocomputing/rincanter
   "1.0.0-SNAPSHOT"
   :description "Clojure/R integration using rosuda JRIEngine"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [incanter "1.2.3"]]
-  :native-dependencies [[jriengine "0.8.4"]]
-  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
-                     [native-deps "1.0.5"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [org.clojure/core.incubator "0.1.2"]
+                 [incanter "1.3.0"]
+                 [REngine/REngine "0.5-5"]
+                 [JRIEngine/JRIEngine "0.5-5"]]
+  :repositories {"snapshots" "http://nexus:8081/nexus/content/repositories/snapshots"
+                 "releases"  "http://nexus:8081/nexus/content/repositories/releases"}
   :autodoc {:name "rincanter"
             :description "Clojure/R integration using rosuda JRIEngine"
             :page-title "Rincanter API documentation"
